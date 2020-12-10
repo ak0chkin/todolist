361836bdf080287827f85908c63c3af0c55bcd7d
ak0chkin <kochkin@protonmail.com>
2020-12-11 02:34:41 +0500

    heroku

--- api/server.js
+++ api/server.js
@@ -1,18 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require("cors");
+const path = require('path');
 
 const app = express();
-
-const corsOptions = {
-    origin: "http://localhost:8081"
-};
-
-app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.get('*', (req, res) => {  res.sendFile(path.join(__dirname, '../client/build/index.html'));});
 
 const db = require("./app/models");
 
@@ -27,7 +25,7 @@ app.get("/", (request, response) => {
 require('./app/routes/auth.routes')(app);
 require('./app/routes/board.routes')(app);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
--- client/src/services/board.service.js
+++ client/src/services/board.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import authHeader from "./auth-header";
 
-const API_URL = 'http://localhost:8080/api/board/';
+const API_URL = '/api/board/';
 
 class BoardService {
     createTask({title, description, expiresAt, priority, performer}) {
