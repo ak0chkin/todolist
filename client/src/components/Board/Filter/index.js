9ed07ff5cf82543b38fac742d7cc7752e0a657f3
ak0chkin <kochkin@protonmail.com>
2020-12-11 14:03:32 +0500

    defaultChecked

--- client/src/components/Board/Filter/index.js
+++ client/src/components/Board/Filter/index.js
@@ -2,15 +2,15 @@ import React from "react";
 import {Dropdown, FormCheck} from "react-bootstrap";
 import "./index.css";
 
-const CustomItem = ({type, id, label, name, className, style, handleFilter}) => (
-    <FormCheck type={type} id={id} name={name} label={label} onChange={handleFilter} className={className}
+const CustomItem = ({type, id, label, name, defaultChecked, className, style, handleFilter}) => (
+    <FormCheck type={type} id={id} name={name} label={label} defaultChecked={defaultChecked} onChange={handleFilter} className={className}
                style={style}/>
 )
 
 function TaskFilter(props) {
     const {performers, handleFilter} = props;
     const performerCheckboxes = performers.map(item => (
-        <Dropdown.Item key={item} as={CustomItem} type="checkbox" id={item} name="performer" label={item}
+        <Dropdown.Item key={item} as={CustomItem} type="checkbox" id={item} name="performer" label={item} defaultChecked={false}
                        handleFilter={handleFilter}/>
     ));
 
@@ -22,13 +22,13 @@ function TaskFilter(props) {
                 </Dropdown.Toggle>
 
                 <Dropdown.Menu>
-                    <Dropdown.Item as={CustomItem} type="radio" id="all" name="expiresAt" label="Все"
+                    <Dropdown.Item as={CustomItem} type="radio" id="all" name="expiresAt" label="Все" defaultChecked={true}
                                    handleFilter={handleFilter}/>
-                    <Dropdown.Item as={CustomItem} type="radio" id="today" name="expiresAt" label="Сегодня"
+                    <Dropdown.Item as={CustomItem} type="radio" id="today" name="expiresAt" label="Сегодня" defaultChecked={false}
                                    handleFilter={handleFilter}/>
-                    <Dropdown.Item as={CustomItem} type="radio" id="week" name="expiresAt" label="Неделя"
+                    <Dropdown.Item as={CustomItem} type="radio" id="week" name="expiresAt" label="Неделя" defaultChecked={false}
                                    handleFilter={handleFilter}/>
-                    <Dropdown.Item as={CustomItem} type="radio" id="future" name="expiresAt" label="Будущее"
+                    <Dropdown.Item as={CustomItem} type="radio" id="future" name="expiresAt" label="Будущее" defaultChecked={false}
                                    handleFilter={handleFilter}/>
                 </Dropdown.Menu>
             </Dropdown>
