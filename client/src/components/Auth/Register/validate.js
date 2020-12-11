2773c339d243f1515241eed61f2f5807988786fe
ak0chkin <kochkin@protonmail.com>
2020-12-11 12:09:54 +0500

    Валидания имени пользователя и пароля

--- client/src/components/Auth/Register/validate.js
+++ client/src/components/Auth/Register/validate.js
@@ -9,8 +9,14 @@ const validate = values => {
     if (!values['username']) {
         errors['username'] = 'Имя пользователя не может быть пустым';
     }
+    if(!(/^[\w_]{3,20}$/).test(values['username'])) {
+        errors['username'] = 'Используйте буквы латинского алфавита, цифры, дефисы и подчёркивания (от 3 до 20 символов)'
+    }
     if (!values['password']) {
         errors['password'] = 'Пароль не может быть пустым';
+    }
+    if(!(/^[\w_]{3,20}$/).test(values['password'])) {
+        errors['password'] = 'Используйте буквы латинского алфавита, цифры, дефисы и подчёркивания (от 3 до 20 символов)'
     }
 
     return errors;
